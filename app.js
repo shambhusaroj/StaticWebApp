626589156d4bf5103baddc5b5438ff7559d0b117
shambhusaroj <shambhusaroj@gmail.com>
2024-03-25 15:13:26 +0530

    cors error

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require("cors");
 const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/initiateRequest1', (req, res) => {
+app.post('/initiateRequest', (req, res) => {
   const { data } = req.body;
-  
+   res.set('Access-Control-Allow-Origin', '*');
+   res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   res.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
   fs.appendFile('output.txt', data + '\n', (err) => {
     if (err) {
       console.error(err);
